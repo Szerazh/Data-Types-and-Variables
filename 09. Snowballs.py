3dbc05ef92736bff8376b27145512702f7b59fc7
Viktoriya Marinova <luhasehuna@gmail.com>
2024-10-05 14:10:53 +0300

    Data Types, Variables

--- 09. Snowballs.py
+++ 09. Snowballs.py
@@ -3,7 +3,7 @@ max_weight = 0
 max_time = 0
 max_value = 0
 max_quality = 0
-for i in range(1, number_of_snowballs + 1):
+for n in range(1, number_of_snowballs + 1):
     snowball_weight = int(input())
     time_needed = int(input())
     quality_snowball = int(input())
